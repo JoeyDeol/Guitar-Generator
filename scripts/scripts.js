b84c8ac0a1d5e3e02645d810a6b2f3b6840fc917
Joey Deol <gurjothdeol@gmail.com>
2018-02-06 17:25:01 -0500

    Initial Commit

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+// I will design a landing page that has a button to start the guitar generator. This button will have an event watcher on it, that once triggered, will disappear the landing portion of the site and then bring up the guitar generator.
+    // This can probably be achieved through toggling classes of both the landing div and the generator div, switching them between display:none and display:block.
+
+//Next I will make it so that in the guitar generator there are 4 categories (this could scale up to more) and they have a few options each. 
+    // each of the options will be retrieved as userInputs, and depending on which inputs are picked, I will try to run .filter() array methods to use the inputs to pear down the possible outputs. 
+
+// At the bottom of the generator page there will be an submit button, which will have another event watcher that will do a similar toggling of classes to the first button, but will instead toggle only for the generator section and the results section.
+
+// The final page will have the filtered result and a few extra buttons to show my personal choice and ryan's personal choice in guitar as well as a quiz again button to go back to the landing/starting section of the generator.
